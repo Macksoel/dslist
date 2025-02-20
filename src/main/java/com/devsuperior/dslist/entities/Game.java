5dac29c81a22aafc8981d093190815d60d35cebe
maxtrunder <maxtrunder@hotmail.com>
2025-02-20 16:22:28 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -22,10 +22,15 @@ public class Game {
 	
 	@Column(name = "game_year")
 	private Integer year;
-	private String genger;
-	private String platform;
+	private String genre;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT") 
 	private String longDescription;
 	
 	
@@ -33,13 +38,14 @@ public class Game {
 	}
 
 
-	public Game(Long id, String title, Integer year, String genger, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
-		this.genger = genger;
-		this.platform = platform;
+		this.genre = genre;
+		this.platforms = platforms;
+		this.score = score ;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -76,23 +82,33 @@ public class Game {
 	}
 
 
-	public String getGenger() {
-		return genger;
+	public String getGenre() {
+		return genre;
 	}
 
 
-	public void setGenger(String genger) {
-		this.genger = genger;
+	public void setGenge(String genger) {
+		this.genre = genger;
 	}
 
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
+	}
+	
+	
+	public Double getScore() {
+		return score;
 	}
 
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
 	}
 
 
