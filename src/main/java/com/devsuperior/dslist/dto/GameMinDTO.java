d0ab74fd440fbe25ce0d0c5fa4f483a6570300b0
maxtrunder <maxtrunder@hotmail.com>
2025-02-27 16:55:22 -0300

    Homolog ok

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -34,7 +34,7 @@ public GameMinDTO(GameMinProjection projection) {
 			
 			id = projection.getId();
 		    title = projection.getTitle();
-			year = projection.getYear();
+			year = projection.getGameYear();
 			imgUrl = projection.getImgUrl();
 			shortDescription = projection.getShortDescription();
 		}
