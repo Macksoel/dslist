0628d5747d534ca61b54af174324cb30ccaa1970
maxtrunder <maxtrunder@hotmail.com>
2025-02-27 14:33:20 -0300

    GameListController, GameMinProjection

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -19,6 +22,8 @@ public class GameListController {
 	private GameListService gameListService;
 	
 	
+	@Autowired
+	private GameService gameService;
 
 	
 	@GetMapping
@@ -26,4 +31,13 @@ public class GameListController {
 		List<GameListDTO> result = gameListService.FindAll();
 		return result;
 	}
+	
+	
+	@GetMapping(value = "/{listId}/games")
+	public List<GameMinDTO> findByList(@PathVariable Long listId){
+		List<GameMinDTO> result = gameService.FindByList(listId);
+		return result;
+	}
+	
+	
 }
